419f78a8df533a18e860f4a5e1d3ebf63858c710
ayaangupta <89456815+ayaangupta@users.noreply.github.com>
2022-11-06 11:00:11 +1100

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,8 @@
+function add_user(){
+player1 = document.getElementById("player1_name_input").value;
+player2 = document.getElementById("player2_name_input").value;
+localStorage.setItem("player1", player1);
+localStorage.setItem("player2", player2);
+window.location = "quiz_game_page.html"
+}
+
